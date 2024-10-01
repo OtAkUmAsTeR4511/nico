8f374da5260cbc8001eb86c6a08ca07de581dd71
OtAkUmAsTeR4511 <a.nicolasriehl@icea.edu.ar>
2024-10-01 19:24:26 +0000

    ñ

--- nicolas.py
+++ nicolas.py
@@ -1,15 +1,15 @@
 x=0
 y=0
 def arriba():
-    
-    while True:
-        if arriba():
-            Y=Y-1
-            if abajo():
-                y=y+1
-                if derecha():
-                    x=x+1
-                    if izquierda():
-                        x=x-1
     return True
+while True:
+    if arriba():
+        Y=Y-1
+    if abajo():
+        y=y+1
+    if derecha():
+        x=x+1
+    if izquierda():
+        x=x-1
+    if
 
